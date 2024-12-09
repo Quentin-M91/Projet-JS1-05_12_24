d4384c78a412108d1f3719e56e4c57502331d5f2
Quentin M <quentinmauffetsanchez@gmail.com>
2024-12-09 13:54:49 +0100

    page 1 changés et fonctionnelle

--- JS/script.js
+++ JS/script.js
@@ -1,51 +1,42 @@
-// fetch ('https://jsonplaceholder.typicode.com/posts')
-//     .then((response) => response.json())
-//     .then(json => console.log(json));
+document.addEventListener("DOMContentLoaded", async () => {
+    const container = document.getElementById("posts-container");
 
-// fetch('https://jsonplaceholder.typicode.com/posts')
-//     .then(response => {
-//         let userArray = response;
-//         response.json().then(data => {
-//             let userArray = data;
-//             console.dir(userArray)
+    try {
+        const posts = await fetch("https://jsonplaceholder.typicode.com/posts").then(res => res.json());
+        for (const post of posts) {
+            const user = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`).then(res => res.json());
+            const postElement = document.createElement("article");
+            postElement.classList.add("product-item");
 
-//             // extraction d'un user
-//             let user = userArray[0];
-//             console.log(user);
-//         })
-//     })
+            const title = document.createElement("h3");
+            title.textContent = post.title;
+            postElement.appendChild(title);
 
-try {
-    // On récupère dans un appel HTTP
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    let users = await response.json();
-    console.log(users);
-    // Récupération des 100 users
-    for (let i = 0; i < 100; i++) {
-        // L'user de l'itération
-        let user = users[i];
-        // Crée un élément dans le DOM
-        let userElement = document.createElement("p")
-        // On injecte du texte dans l'élément 
-        userElement.textContent = `Nom d'utilisateur : ${user.userId} titre : ${user.title} Contenu : ${user.body}`
-        // On rattache l'élément au DOM
-        document.body.appendChild(userElement);
-    } 
-} catch (err) {
-    console.log("erreur sur cette partie");
-}
+            const body = document.createElement("p");
+            body.textContent = post.body;
+            postElement.appendChild(body);
 
+            const username = document.createElement("p");
+            username.textContent = `User ID: ${user.id}`;
+            postElement.appendChild(username);
 
+            const userLink = document.createElement("a");
+            userLink.textContent = "Page 2 - Utilisateur";
+            userLink.href = `../HTML/user-posts.html?userId=${post.userId}`;
+            userLink.classList.add("button");
+            postElement.appendChild(userLink);
 
-// const loadHeader = () => {
-//     fetch('https://jsonplaceholder.typicode.com/posts')
-//     .then(res => {
-//         return res.text()
-//     })
-//     .then(data => {
-//         document.querySelector('#header__part').innerHTML = data;
-//     })
-//     console.log('header');
-// }
-// loadHeader();
+            const postLink = document.createElement("a");
+            postLink.textContent = "Page 3 - Le post de l'user";
+            postLink.href = `../HTML/page3.html?postId=${post.id}`;
+            postLink.classList.add("button");
+            postElement.appendChild(postLink);
 
+            container.appendChild(postElement);
+        }
+    } catch (error) {
+
+        container.textContent = "Une erreur s'est produite lors du chargement des posts.";
+        console.error(error);
+    }
+});
